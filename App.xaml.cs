3cb9a664e4225b59fdc9640ebbbe0519706cc45d
Wilastian <sterkhov.sasha@mail.ru>
2023-05-15 13:25:08 +0500

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Shop
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static Orders CurrentOrder { get; set; } = null;
+       
+    }
+}
